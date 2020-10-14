f3cb1184917e71a9cb73764f1d9d93ee3f1213f5
Hubert Gawlowski <hubertgaw@gmail.com>
2020-10-15 01:20:14 +0200

    Prepare layout

--- app/src/main/java/com/example/equation_calculator/MainActivity.java
+++ app/src/main/java/com/example/equation_calculator/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.equation_calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView x;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        x = (TextView) findViewById(R.id.xSymbol1);
+        x.setText(Html.fromHtml("x<sup><small>2</small></sup>"));
     }
 }
