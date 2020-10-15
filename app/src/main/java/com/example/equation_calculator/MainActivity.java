3fa0799c06fc59449ccef9dc2d47c0e96b119906
Hubert Gawlowski <hubertgaw@gmail.com>
2020-10-15 02:20:47 +0200

    Add functionallity to compute simple equation (withoout a=0 and delta<0)

--- app/src/main/java/com/example/equation_calculator/MainActivity.java
+++ app/src/main/java/com/example/equation_calculator/MainActivity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView x;
+    private TextView x, result1, result2, deltaText;
+    private EditText a, b, c;
+    private Button computeButton;
+    public static DecimalFormat DECIMAL_FORMATTER;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +27,40 @@ public class MainActivity extends AppCompatActivity {
 
         x = (TextView) findViewById(R.id.xSymbol1);
         x.setText(Html.fromHtml("x<sup><small>2</small></sup>"));
+
+        DecimalFormatSymbols symbols = new DecimalFormatSymbols(Locale.US);
+        symbols.setDecimalSeparator('.');
+        DECIMAL_FORMATTER = new DecimalFormat("#.000", symbols);
+
+        computeButton = (Button) findViewById(R.id.computeButton);
+
+        computeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                a = (EditText) findViewById(R.id.aFactor);
+                b = (EditText) findViewById(R.id.bFactor);
+                c = (EditText) findViewById(R.id.cFactor);
+
+                result1 = (TextView) findViewById(R.id.result1);
+                result2 = (TextView) findViewById(R.id.result2);
+                deltaText = (TextView) findViewById(R.id.deltaTextView);
+
+                double aNum = Integer.parseInt(a.getText().toString());
+                double bNum = Integer.parseInt(b.getText().toString());
+                double cNum = Integer.parseInt(c.getText().toString());
+
+                double delta = bNum*bNum - 4.0*aNum*cNum;
+
+                double deltaSqrt = Math.sqrt(delta);
+
+                double x1 = (-bNum - deltaSqrt) / (2.0*aNum);
+                double x2 = (-bNum + deltaSqrt) / (2.0*aNum);
+
+                result1.setText(DECIMAL_FORMATTER.format(x1));
+                result2.setText(DECIMAL_FORMATTER.format(x2));
+                deltaText.setText(DECIMAL_FORMATTER.format(delta));
+            }
+        });
+
     }
 }
