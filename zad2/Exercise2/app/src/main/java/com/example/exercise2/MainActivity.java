6a626dc4cff67e67fa04f7ce0c64822297972826
Hubert Gawlowski <hubertgaw@gmail.com>
2020-10-27 01:36:38 +0100

    Add functionality Second, google, map and phone with change in manifest

--- zad2/Exercise2/app/src/main/java/com/example/exercise2/MainActivity.java
+++ zad2/Exercise2/app/src/main/java/com/example/exercise2/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.exercise2;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -12,8 +15,24 @@ import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+
+import java.net.URI;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Intent getWebAdressIntent(String address, String type) {
+        Uri uriAdress = Uri.parse(address);
+        return new Intent(type, uriAdress);
+    }
+
+    private void openActivity(String name, String type) {
+        Intent gotoActivity = getWebAdressIntent(name, type);
+        if (gotoActivity.resolveActivity(getPackageManager()) != null) {
+            startActivity(gotoActivity);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,35 +41,59 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button secondActivityBtn = (Button) findViewById(R.id.secondActivityBtn);
+        secondActivityBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(),SecondActivity.class);
+                startActivity(startIntent);
+            }
+        });
+
+        Button googleBtn = (Button)findViewById(R.id.googleBtn);
+        googleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity("http://www.google.com", Intent.ACTION_VIEW);
+            }
+        });
+
+        Button mapBtn = (Button)findViewById(R.id.mapBtn);
+        mapBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity("geo:0,0?q=1600+Amphitheatre+Parkway,+Mountain+View,+California", Intent.ACTION_VIEW);
+            }
+        });
+
+        Button phoneBtn = (Button)findViewById(R.id.phoneBtn);
+        phoneBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity("tel:5551234", Intent.ACTION_DIAL);
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 }
