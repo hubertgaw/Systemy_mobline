cd0382c820ae789be2ed9c9359e3cfa92027b188
Hubert Gawlowski <hubertgaw@gmail.com>
2020-11-09 20:34:24 +0100

    Make DrawActivity FullScreen and without Title

--- zad3/Exercise3/app/src/main/java/com/example/exercise3/DrawActivity.java
+++ zad3/Exercise3/app/src/main/java/com/example/exercise3/DrawActivity.java
@@ -4,10 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import static android.view.Window.FEATURE_NO_TITLE;
+import static android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN;
+
 public class DrawActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        //requestWindowFeature(FEATURE_NO_TITLE);
+        getWindow().setFlags(FLAG_FULLSCREEN, FLAG_FULLSCREEN);
+
     }
 }
