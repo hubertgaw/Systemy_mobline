e809132492d52119edfa18bba4b99f41bfac6343
Hubert Gawlowski <hubertgaw@gmail.com>
2020-11-09 20:12:20 +0100

    Add button and DrawActivity

---
+++ zad3/Exercise3/app/src/main/java/com/example/exercise3/DrawActivity.java
@@ -0,0 +1,13 @@
+package com.example.exercise3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DrawActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
