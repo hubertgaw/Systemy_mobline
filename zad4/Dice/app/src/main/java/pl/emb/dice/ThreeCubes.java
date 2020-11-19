b18e76dca20565811f7a02430565c40b7467b731
Hubert Gawlowski <hubertgaw@gmail.com>
2020-11-19 14:24:26 +0100

    Make two cubes and three cubes in graphic form

--- zad4/Dice/app/src/main/java/pl/emb/dice/ThreeCubes.java
+++ zad4/Dice/app/src/main/java/pl/emb/dice/ThreeCubes.java
@@ -2,13 +2,16 @@ package pl.emb.dice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ThreeCubes extends AppCompatActivity {
 
     private Accelerometer accelerometer;
     private TextView mNumber1, mNumber2, mNumber3;
+    private ImageView cube1, cube2, cube3;
     Tools tools;
 
 
@@ -20,6 +23,11 @@ public class ThreeCubes extends AppCompatActivity {
         mNumber1 = (TextView)findViewById(R.id.threeNumbers1);
         mNumber2 = (TextView)findViewById(R.id.threeNumbers2);
         mNumber3 = (TextView)findViewById(R.id.threeNumbers3);
+
+        cube1 = (ImageView)findViewById(R.id.threeCubesImage1);
+        cube2 = (ImageView)findViewById(R.id.threeCubesImage2);
+        cube3 = (ImageView)findViewById(R.id.threeCubesImage3);
+
         accelerometer = new Accelerometer(this);
         tools = new Tools();
         accelerometer.setListener(new Accelerometer.Listener() {
@@ -28,12 +36,15 @@ public class ThreeCubes extends AppCompatActivity {
                 if (acceleration > accelerometer.getShakeThreshold()) {
                     int randomNum1 = tools.generateRandomNumber();
                     mNumber1.setText(Integer.toString(randomNum1));
+                    tools.setImage(cube1,randomNum1);
 
                     int randomNum2 = tools.generateRandomNumber();
                     mNumber2.setText(Integer.toString(randomNum2));
+                    tools.setImage(cube2,randomNum2);
 
                     int randomNum3 = tools.generateRandomNumber();
                     mNumber3.setText(Integer.toString(randomNum3));
+                    tools.setImage(cube3,randomNum3);
                 }
             }
         });
--- zad4/Dice/app/src/main/java/pl/emb/dice/TwoCubes.java
+++ zad4/Dice/app/src/main/java/pl/emb/dice/TwoCubes.java
@@ -3,12 +3,14 @@ package pl.emb.dice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class TwoCubes extends AppCompatActivity {
 
     private Accelerometer accelerometer;
     private TextView mNumber1, mNumber2;
+    private ImageView cube1, cube2;
     Tools tools;
 
 
@@ -19,6 +21,8 @@ public class TwoCubes extends AppCompatActivity {
 
         mNumber1 = (TextView)findViewById(R.id.twoNumbers1);
         mNumber2 = (TextView)findViewById(R.id.twoNumbers2);
+        cube1 = (ImageView)findViewById(R.id.twoCubesImage1);
+        cube2 = (ImageView)findViewById(R.id.twoCubesImage2);
         accelerometer = new Accelerometer(this);
         tools = new Tools();
         accelerometer.setListener(new Accelerometer.Listener() {
@@ -27,9 +31,11 @@ public class TwoCubes extends AppCompatActivity {
                 if (acceleration > accelerometer.getShakeThreshold()) {
                     int randomNum1 = tools.generateRandomNumber();
                     mNumber1.setText(Integer.toString(randomNum1));
+                    tools.setImage(cube1, randomNum1);
 
                     int randomNum2 = tools.generateRandomNumber();
                     mNumber2.setText(Integer.toString(randomNum2));
+                    tools.setImage(cube2, randomNum2);
                 }
             }
         });
