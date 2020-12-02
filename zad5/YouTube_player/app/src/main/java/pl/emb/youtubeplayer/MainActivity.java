c2b5ffa6bbc519ac7b6f95e35f4205d76ce2107e
Hubert Gawlowski <hubertgaw@gmail.com>
2020-12-02 01:14:32 +0100

    Version for grade 3 - basic YouTube app

--- zad5/YouTube_player/app/src/main/java/pl/emb/youtubeplayer/MainActivity.java
+++ zad5/YouTube_player/app/src/main/java/pl/emb/youtubeplayer/MainActivity.java
@@ -1,14 +1,39 @@
 package pl.emb.youtubeplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
+import android.view.View;
+import android.widget.Button;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+public class MainActivity extends YouTubeBaseActivity {
+    YouTubePlayerView youTubePlayerView;
+    private YouTubePlayer.OnInitializedListener onInitializedListener = new
+            YouTubePlayer.OnInitializedListener() {
+                @Override
+                public void onInitializationSuccess(YouTubePlayer.Provider provider,
+                                                    YouTubePlayer youTubePlayer, boolean b) {
+                    youTubePlayer.loadVideo("https://www.youtube.com/watch?v=HX3p7-bN1BI");
+                }
+                @Override
+                public void onInitializationFailure(YouTubePlayer.Provider provider,
+                                                    YouTubeInitializationResult youTubeInitializationResult) {
+                }
+            };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button2 = (Button)findViewById(R.id.playVideoBtn);
+        youTubePlayerView = findViewById(R.id.youtube_view);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                youTubePlayerView.initialize("AIzaSyCdkB_O6Jye_isXs3iiBf7qMwewwZ-WV7M", onInitializedListener);
+            }
+        });
     }
 }
