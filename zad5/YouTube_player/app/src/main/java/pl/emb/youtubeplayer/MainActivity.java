2847b6f738ddcd712892b09f39a6047156dae133
Hubert Gawlowski <hubertgaw@gmail.com>
2020-12-03 14:51:06 +0100

    For the grade 5 - implement pause, stop, play buttons

--- zad5/YouTube_player/app/src/main/java/pl/emb/youtubeplayer/MainActivity.java
+++ zad5/YouTube_player/app/src/main/java/pl/emb/youtubeplayer/MainActivity.java
@@ -1,11 +1,9 @@
 package pl.emb.youtubeplayer;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -15,18 +13,27 @@ import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Set;
 
 public class MainActivity extends YouTubeBaseActivity
-        implements AdapterView.OnItemSelectedListener {
+        implements AdapterView.OnItemSelectedListener{
 
     YouTubePlayerView youTubePlayerView;
     private YouTubePlayer mYouTubePlayer;
-
     private String movieURL;
+    private boolean isPaused = false;
+    LinkedHashMap<String, String> moviesMap = new LinkedHashMap<String, String>();
+    private YouTubePlayer.OnInitializedListener onInitializedListener;
+
+    public boolean getPaused() {
+        return isPaused;
+    }
+
+    public void setPaused(boolean paused) {
+        isPaused = paused;
+    }
 
     public void setMovieURL(String movieURL) {
         this.movieURL = movieURL;
@@ -35,9 +42,7 @@ public class MainActivity extends YouTubeBaseActivity
     public String getMovieURL() {
         return movieURL;
     }
-    LinkedHashMap<String, String> moviesMap = new LinkedHashMap<String, String>();
 
-    private YouTubePlayer.OnInitializedListener onInitializedListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,25 +50,38 @@ public class MainActivity extends YouTubeBaseActivity
         setContentView(R.layout.activity_main);
         youTubePlayerSetup();
         youTubePlayerView = findViewById(R.id.youtube_view);
-        moviesMap.put("Program meczowy #66", "5IQ_X1ZmgGw");
-        moviesMap.put("Program meczowy #65", "KD8zBYobyw");
-        moviesMap.put("Ośma liga mistrzow odc. 17", "coBucG1-8h0");
+        createTitlesArray();
+        spinnerInit(createTitlesArray());
+    }
+
+    private String[] createTitlesArray() {
+        moviesMap.put("Bohemian Rhapsody", "fJ9rUzIMcZQ");
+        moviesMap.put("I Want to Break Free", "f4Mc-NYPHaQ");
+        moviesMap.put("We Will Rock You", "-tJYN-eG1zk-8h0");
+        moviesMap.put("Radio Ga Ga", "azdwsXLmrHE");
         Set<String> moviesKeysSet = moviesMap.keySet();
         List<String> moviesTitlesList = new ArrayList<String>(moviesKeysSet);
         String[] moviesTitlesArray = new String[moviesTitlesList.size()];
         moviesTitlesList.toArray(moviesTitlesArray);
+        return moviesTitlesArray;
+    }
 
+    private void spinnerInit(String[] moviesTitlesArray) {
         Spinner movieSpinner = (Spinner) findViewById(R.id.movieSpinner);
         ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(this,
                 android.R.layout.simple_spinner_dropdown_item, moviesTitlesArray);
         movieSpinner.setAdapter(adapter);
         movieSpinner.setOnItemSelectedListener(this);
-
     }
 
     public void onClickPlayVideo(View view) {
         try {
-            mYouTubePlayer.loadVideo(getMovieURL());
+            if (getPaused()) {
+                mYouTubePlayer.play();
+            }
+            else {
+                mYouTubePlayer.loadVideo(getMovieURL());
+            }
         } catch (Throwable T) {
             Toast.makeText(getApplicationContext(), "Error during Video load",
                     Toast.LENGTH_LONG).show();
@@ -110,4 +128,18 @@ public class MainActivity extends YouTubeBaseActivity
         youTubePlayerView.initialize(Config.getYoutubeApiKey(), onInitializedListener);
 
     }
+
+    public void onClickPauseBtn(View view) {
+        mYouTubePlayer.pause();
+        setPaused(true);
+    }
+
+    public void onClickStopBtn(View view) {
+        mYouTubePlayer.release();
+        mYouTubePlayer = null;
+        youTubePlayerSetup();
+        setPaused(false);
+
+    }
+
 }
